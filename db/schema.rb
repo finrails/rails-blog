8ecdaad2a81b594f82d94ca973156583b33295a9
Vitor Souza <macacofinn@yahoo.com>
2022-04-15 16:05:20 -0400

    add: post comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_14_035706) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_15_150650) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "author"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "author"
