7ad514cd226ef375e7599184c3907ac581de55f4
Vitor Souza <macacofinn@yahoo.com>
2022-04-15 17:33:55 -0400

    upt: comment create system work

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+  has_many :comments
+
+  validates :author, presence: true, length: { minimum: 4, maximum: 24 }
+  validates :title, presence: true, length: { minimum: 10, maximum: 30 }
+  validates :body, presence: true, length: { minimum: 10, maximum: 200 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'welcomes#index'
   resources :welcomes, only: :index
   resources :posts do
-    resources :comments, only: %i[create new destroy]
+    resources :comments, only: %i[index create new destroy]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
