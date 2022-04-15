3ee50647d30524c1d5bf712dcdb614f9109a81ef
Vitor Souza <macacofinn@yahoo.com>
2022-04-15 10:43:08 -0400

    add: destroy route

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,21 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      return render :edit, status: :unprocessable_entity
+    end
+
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
   def new
     @post = Post.new
   end
@@ -19,6 +34,13 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path, status: :see_other
   end
 
   private
