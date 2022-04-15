b65d6f611861885f5a33b7689ddf78a8a7b8899f
Vitor Souza <macacofinn@yahoo.com>
2022-04-14 23:25:44 -0400

    upt: some back buttons

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,29 @@
 class PostsController < ApplicationController
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def index
     @posts = Post.all
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:author, :title, :body)
   end
 end
