d3a624d21d871b5cd3cd61b34fd30506b76cbdf1
Vitor Souza <macacofinn@yahoo.com>
2022-04-15 22:21:58 -0400

    fix: comment validates min and max size

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :post
 
-  validates :author, presence: true, length: { minimum: 4, maximum: 15}
-  validates :body, presence: true, length: { minimum: 15, maximum: 60}
+  validates :author, presence: true, length: { minimum: 4, maximum: 20}
+  validates :body, presence: true, length: { minimum: 5, maximum: 120}
 end
